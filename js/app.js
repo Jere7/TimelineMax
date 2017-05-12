d2bc493a5c127b39b2efe376e6fe5b6bed4dacb4
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-12 13:13:15 -0600

    scale and opacity to js/app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ $(window).load(function(){
 function init(){
     var toolTimeline = new TimelineMax();
     var duration = .5;
-    toolTimeline.from('#toolBox', duration, {y:-100, ease:Linear.easeInOut});
+
+    toolTimeline.from('.mainTitle', duration, {opacity:0,scale:25, ease:Linear.easeInOut});
     // toolTimeline.to('#weightLifter', duration, {y:-100, ease:Bounce.easeInOut});
     // toolTimeline.to('#crazy', duration, {y:-100, ease:Elastic.easeInOut});
     // toolTimeline.to('#leaf', duration, {y:-100, ease:Back.easeInOut});
