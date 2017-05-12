e37c311970c2df2ba2c20c865e775cdff043ec84
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-12 13:02:58 -0600

    comments a few part of animation just leave a Timeline.from

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ $(window).load(function(){
 function init(){
     var toolTimeline = new TimelineMax();
     var duration = .5;
-    toolTimeline.to('#toolBox', duration, {y:-100, ease:Linear.easeInOut});
-    toolTimeline.to('#weightLifter', duration, {y:-100, ease:Bounce.easeInOut});
-    toolTimeline.to('#crazy', duration, {y:-100, ease:Elastic.easeInOut});
-    toolTimeline.to('#leaf', duration, {y:-100, ease:Back.easeInOut});
+    toolTimeline.from('#toolBox', duration, {y:-100, ease:Linear.easeInOut});
+    // toolTimeline.to('#weightLifter', duration, {y:-100, ease:Bounce.easeInOut});
+    // toolTimeline.to('#crazy', duration, {y:-100, ease:Elastic.easeInOut});
+    // toolTimeline.to('#leaf', duration, {y:-100, ease:Back.easeInOut});
 
     // $("#start").click(function(){
     //     toolTimeline.play();
