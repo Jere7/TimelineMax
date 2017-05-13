75b60730e4a62bc8c8761879283f3d6449758f76
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-13 11:49:19 -0600

    a few comments to explain the animations properties

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
 
 
 
+
 $(window).on('load', function(){
     init();
 });
 
 
 function init(){
+    // sequence TimelineMax
     var toolTimeline = new TimelineMax();
     var duration = .5;
 
+    // sequence TimelineMax.from(ease:Linear.easeInOut)
+
     toolTimeline.from('.mainTitle', duration, {opacity: 0,scale:25, ease:Linear.easeInOut},1.5);
     toolTimeline.from('.title', duration, {opacity: 0,scale:25, ease:Linear.easeInOut} ,1.5);
-    toolTimeline.to('.superman img', duration, {left: 0, ease:Linear.easeInOut});
-    toolTimeline.to('.mainTitle', duration, {right: -50, ease:Linear.easeInOut});
+    
+    // sequence TimelineMax.to left:0-right:-50 (ease:Back.easeInOut)
 
+    toolTimeline.to('.superman img', duration, {left: 0, ease:Back.easeInOut});
+    toolTimeline.to('.mainTitle', duration, {right: -50, ease:Back.easeInOut}, .7);
 
+    // sequence TimelineMax.from
+                 
     toolTimeline.from('#toolBox', duration, {opacity: 0,scale:5, ease:Linear.easeInOut}, .5);
     toolTimeline.from('#leaf', duration, {opacity: 0,scale:25, ease:Linear.easeInOut}, .5);
     toolTimeline.from('#weightLifter', duration, {opacity: 0,scale:25, ease:Linear.easeInOut},.5);
     toolTimeline.from('#crazy', duration, {opacity: 0,scale:25, ease:Linear.easeInOut},.5);
     toolTimeline.from('#supermeme', duration, {opacity: 0,scale:25, ease:Linear.easeInOut},1);
 
+    // jquery selector to buttons class.controlButtons
 
     // $("#start").click(function(){
     //     toolTimeline.play();
