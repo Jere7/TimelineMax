a68638cb8121107bd7f4e213e6e0c1399530b3d6
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-13 11:58:05 -0600

    more commets explaining code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
-
-
+// start the function $(window).on('load', function(){
+// });
 
 $(window).on('load', function(){
     init();
@@ -8,21 +8,21 @@ $(window).on('load', function(){
 
 
 function init(){
-    // sequence TimelineMax
+    // Sequence TimelineMax
     var toolTimeline = new TimelineMax();
     var duration = .5;
 
-    // sequence TimelineMax.from(ease:Linear.easeInOut)
-
+    // Sequence TimelineMax.from(ease:Linear.easeInOut)
+    // 
     toolTimeline.from('.mainTitle', duration, {opacity: 0,scale:25, ease:Linear.easeInOut},1.5);
     toolTimeline.from('.title', duration, {opacity: 0,scale:25, ease:Linear.easeInOut} ,1.5);
     
-    // sequence TimelineMax.to left:0-right:-50 (ease:Back.easeInOut)
+    // Sequence TimelineMax.to left:0-right:-50 (ease:Back.easeInOut)
 
     toolTimeline.to('.superman img', duration, {left: 0, ease:Back.easeInOut});
     toolTimeline.to('.mainTitle', duration, {right: -50, ease:Back.easeInOut}, .7);
 
-    // sequence TimelineMax.from
+    // Sequence TimelineMax.from
                  
     toolTimeline.from('#toolBox', duration, {opacity: 0,scale:5, ease:Linear.easeInOut}, .5);
     toolTimeline.from('#leaf', duration, {opacity: 0,scale:25, ease:Linear.easeInOut}, .5);
@@ -30,7 +30,7 @@ function init(){
     toolTimeline.from('#crazy', duration, {opacity: 0,scale:25, ease:Linear.easeInOut},.5);
     toolTimeline.from('#supermeme', duration, {opacity: 0,scale:25, ease:Linear.easeInOut},1);
 
-    // jquery selector to buttons class.controlButtons
+    // Jquery selector to buttons class.controlButtons
 
     // $("#start").click(function(){
     //     toolTimeline.play();
