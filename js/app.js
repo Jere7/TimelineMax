74036bb9c0187774b590e2ba469ccf5b06c4514a
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-15 13:10:38 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -20,15 +20,15 @@ function init(){
     
     // Sequence TimelineMax.to left:0-right:-50 (ease:Back.easeInOut)
 
-    toolTimeline.to('.superman img', duration, {left: 0, ease:Back.easeInOut});
+    toolTimeline.to('.superman img', duration, {left: 0, ease:Back.easeOut});
     toolTimeline.to('.mainTitle', duration, {right: -50, ease:Back.easeInOut}, 2);    
     // Sequence TimelineMax.from
                  
-    toolTimeline.from('#toolBox', duration, {opacity: 0,scale: 5, ease:Linear.easeInOut}, 1);
+    toolTimeline.from('#toolBox', duration, {opacity: 0,scale: 5, ease:Linear.easeInOut}, .5);
     toolTimeline.from('#leaf', duration, {opacity: 0,scale: 25, ease:Linear.easeInOut}, .5);
     toolTimeline.from('#weightLifter', duration, {opacity: 0,scale: 25, ease:Linear.easeInOut},.5);
-    toolTimeline.from('#crazy', duration, {opacity: 0,scale: 25, ease:Linear.easeInOut},1);
-    toolTimeline.from('#supermeme', duration, {opacity: 0,scale: 25, ease:Linear.easeInOut},.7);
+    toolTimeline.from('#crazy', duration, {opacity: 0,scale: 25, ease:Linear.easeInOut},.5);
+    toolTimeline.from('#supermeme', duration, {opacity: 0,scale: 25, ease:Linear.easeInOut},.5);
 
    
 
@@ -70,23 +70,23 @@ function startTween(){
          TweenLite.to($("#leaf"), 2, {delay: d, x: 100, y: -30, scale: 3.5,
               onComplete: returnToNormal, onCompleteParams: [$("#leaf")]});            
              
-         TweenLite.to($(".gsapTitle"), 1, {delay: d + 3, x: 0, y: 0, scale: .5,
-              onComplete: returnToNormal, onCompleteParams: [$(".gsapTitle")]});    
+         TweenLite.to($(".gsapTitle"), 1, {delay: d + 3, x: 0, y: 0, scale: 2.5,
+              onComplete: returnTextNormal, onCompleteParams: [$(".gsapTitle")]});    
               
-          TweenLite.to($(".title"), 1, {delay: d + 3, x: 0, y: 0, scale: .5,
-              onComplete: returnToNormal, onCompleteParams: [$(".title")]});      
+          TweenLite.to($(".title"), 1, {delay: d + 3, x: 0, y: 0, scale: 2.5,
+              onComplete: returnTextNormal, onCompleteParams: [$(".title")]});      
 }
 
 function returnToNormal(obj) {    
     if(obj){
-         TweenLite.to(obj, 1.5, {x: 200, y: 180  , scale: 1.5 });       
+         TweenLite.to(obj, 1.5, {x: 200, y: 290  , scale: 1.5 });       
     }
             
 }
 
 function returnTextToNormal() {
-         TweenLite.to(".title",1.5, {x: 0, y: 0, color: "blue", fontSize: "+90" });
-         TweenLite.to(".greenText", 1.5, {x: 600, y: 0, color: "red", fontSize: "+90" });
-         TweenLite.to(".asapReg", 1.5, {x: 600, y: 0, color: "green", fontSize: "+90" });
+         TweenLite.to(".title", 1, {x: 0, y: 0, color: "blue", fontSize: "+90" });
+         TweenLite.to(".greenText", 1, {x: 600, y: 0, color: "red", fontSize: "+90" });
+         TweenLite.to(".asapReg", 1, {x: 600, y: 0, color: "green", fontSize: "+90" });
          
 }  
